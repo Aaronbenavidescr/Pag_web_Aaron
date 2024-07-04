db57806fac7a5a3f8f9633f646a3e8ca6056dc63
Aaronbenavidescr <aaronbenavidescr@gmail.com>
2024-07-03 22:11:03 -0600

    Avance

--- scripts/main.js
+++ scripts/main.js
@@ -16,26 +16,28 @@ document.getElementById("closeMenu").addEventListener("click", function() {
     menu.style.left = "-250px"; // Oculta el menú
 });
 
-document.getElementById("requisitosMatricula").addEventListener("click", function(event) {
-    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-    var imagenMatriculaContainer = document.getElementById("imagenMatriculaContainer");
-    imagenMatriculaContainer.style.display = "block"; // Muestra el contenedor de la imagen
-    var imagenacercadeContainer = document.getElementById("imagenacercadeContainer");
-    imagenacercadeContainer.style.display = "none"; // Oculta la imagen de Acerca de si está visible
-    var menu = document.getElementById("menuDesplegable");
-    menu.style.left = "-250px"; // Oculta el menú
-});
+
 
 document.getElementById("inicio").addEventListener("click", function() {
     location.reload(); // Recarga la página
 });
 
+document.getElementById("requisitosMatricula").addEventListener("click", function(event) {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    var titulorequisitomatricula = document.getElementById("titulorequisitomatricula");
+    var titulorequisitomatriculaPosition = titulorequisitomatricula.offsetTop;
+    window.scrollTo({
+        top: titulorequisitomatriculaPosition,
+        behavior: 'smooth'
+    });
+});
+
 document.getElementById("acercade").addEventListener("click", function(event) {
     event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-    var imagenacercadeContainer = document.getElementById("imagenacercadeContainer");
-    imagenacercadeContainer.style.display = "block"; // Muestra el contenedor de la imagen
-    var imagenMatriculaContainer = document.getElementById("imagenMatriculaContainer");
-    imagenMatriculaContainer.style.display = "none"; // Oculta la imagen de Requisitos de matrícula si está visible
-    var menu = document.getElementById("menuDesplegable");
-    menu.style.left = "-250px"; // Oculta el menú
+    var tituloacercade = document.getElementById("tituloacercade");
+    var tituloacercadePosition = tituloacercade.offsetTop;
+    window.scrollTo({
+        top: tituloacercadePosition,
+        behavior: 'smooth'
+    });
 });
